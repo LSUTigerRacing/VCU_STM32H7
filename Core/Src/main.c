31786b65bf72f89b01afe5efff8df45c4b41c129
LSUAlexBui <alexdbui4@gmail.com>
2026-09-13 00:54:01 -0500

    Feat: CAN decoding + bit extraction function

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -210,6 +210,21 @@ void SystemClock_Config(void)
 
 /* USER CODE BEGIN 4 */
 
+/// @brief Extracts bits from a starting point and takes defined amount
+/// @param data data that needs to be extracted from
+/// @param start_bit starting bit that wants to be extracted from data
+/// @param size number of bits that are going to be extracted
+/// @return Desired bits
+uint64_t extract_bits(uint64_t data, uint8_t start_bit, uint8_t size){
+  if(size > 64 || size == 0){
+    return 0;
+  }
+
+  uint64_t mask;
+
+  mask = (1ULL << size) - 1;
+  return ((data >> start_bit) & mask);
+}
 /* USER CODE END 4 */
 
  /* MPU Configuration */
